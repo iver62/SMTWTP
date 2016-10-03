609bfc7d296436fc528975184954342d16211a0d
Deshayes Pierrick <deshayes@b04p10.fil.univ-lille1.fr>
2016-10-03 10:11:27 +0200

    update

---
+++ src/algorithmes/ILS.java
@@ -0,0 +1,19 @@
+package algorithmes;
+
+import java.util.List;
+
+import models.Neighborhood;
+import models.Ordonnancement;
+
+public class ILS {
+	
+	private String select; // strategie de selection du meilleur voisin 
+	private Ordonnancement initSol; // la solution initiale
+	private List<Neighborhood> voisinages; // liste des voisinages
+	private int n; // la nieme instance
+	
+	public void run() {
+		
+	}
+
+}
